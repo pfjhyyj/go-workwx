85ead4d517ddc2e88a8889b9043ac191c91d521b
chipunderblade <pfjhyyj@qq.com>
2023-10-23 17:24:49 +0800

    feat: add servicer list && add contact list

--- kf.go
+++ kf.go
@@ -1,5 +1,31 @@
 package workwx
 
+// GetKfAccountList 获取客服列表
+func (c *WorkwxApp) GetKfAccountList(offset uint32, limit uint32) ([]KfAccount, error) {
+	resp, err := c.execListKfAccount(reqListKfAccount{
+		Offset: offset,
+		Limit:  limit,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.AccountList, nil
+}
+
+// GetServicerList 获取接待人员列表
+func (c *WorkwxApp) GetServicerList(openKfId string) ([]KfServicer, error) {
+	resp, err := c.execListServicer(reqListServicer{
+		OpenKfId: openKfId,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.ServicerList, nil
+}
+
 // GetKfServiceState 获取微信客服状态
 func (c *WorkwxApp) GetKfServiceState(
 	openKfId string,
